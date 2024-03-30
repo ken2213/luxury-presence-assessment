a1f5fd9ded2f6877a0ad5e2c3e46994fb1fa7d97
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-31 00:40:31 +0800

    IMPLEMENTED: responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,21 +19,24 @@ const Hero = () => {
     <div className='h-[100vh] bg-white'>
       <div className='bg flex flex-col pt-28 gap-y-4 items-center h-full'>
         <div className='w-full text-center'>
-          <h1 className='text-[6rem] font-serif font-semibold text-main tracking-wider'>
-            Services
+          <h1 className='text-[3rem] md:text-[6rem] font-playfair-display font-bold text-main tracking-wider'>
+            SERVICES
           </h1>
         </div>
 
-        <div className='bg-[#f1f4f9] relative z-30 top-12 grid grid-cols-[50%_10%_40%] w-[80%] md:w-[650px] h-[60px] rounded-full shadow-lg'>
-          <div className=' items-center rounded-full pl-4 gap-x-2 grid grid-cols-[10%_80%]'>
+        <div className='bg-[#f1f4f9] relative z-10 top-12 grid grid-cols-[50%_10%_40%] w-[80%] md:w-[650px] h-[60px] rounded-full shadow-lg'>
+          <div className='items-center rounded-full pl-2 sm:pl-4 gap-x-1 sm:gap-x-2 grid grid-cols-[10%_80%]'>
             <div className=''>
-              <Search />
+              <Search 
+                size={20}
+                className='hidden sm:flex' 
+              />
             </div>
 
             <div className='w-[100%]'>
               <input 
                 type="text"
-                className='rounded-full py-2 px-4 shadow-md'
+                className='rounded-full text-[0.8rem] sm:text-[1rem] py-2 px-4 shadow-md w-[100%] sm:w-[100%]'
                 placeholder='Enter property' 
               />
             </div>
@@ -45,9 +48,12 @@ const Hero = () => {
           </div>
 
           <div className='flex justify-center items-center '>
-            <button className='bg-main text-white py-2 px-5 mx-1 text-sm flex items-center gap-x-2 rounded-full shadow-md'>
+            <button className='bg-main text-white py-2 px-2 sm:px-5 mx-1 text-[0.6rem] md:text-sm flex items-center gap-x-2 rounded-full shadow-md line-clamp-4'>
               BOOK AN APPOINTMENT
-              <ArrowRight size={20} />
+              <ArrowRight 
+                size={20}
+                className='hidden sm:flex' 
+              />
             </button>
           </div>
         </div>
