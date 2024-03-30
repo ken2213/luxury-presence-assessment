664a08e7a06e9ce921f6930c650148ee5375bf99
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-31 01:26:17 +0800

    COMMENT: WhyUs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <ProcessSelling />
       <ProcessBuying />
       <RealtorInfo />
-      <WhyUs />
+      {/* <WhyUs /> */}
       <CallToAction />
       <Footer />
     </>
