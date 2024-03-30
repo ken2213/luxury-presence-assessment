f6771e2beaa9d4a3d38a4d454bd4629ae978c3fa
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-31 00:26:04 +0800

    ADDED: meta description |--| COMMENT OUT: Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Services | Pleasanton Real Estate | Julie Hansen Partnership",
-  description: "",
+  description: "An elite group of the East Bay’s most talented and visionary real estate professionals believed buyers and sellers deserved more from their real estate company. More service. More resources. More integrity. More global reach. In a word, more of everything people should expect when they buy or sell their homes.",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         <div className="min-h-screen">
           {children}
         </div>
