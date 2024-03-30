7872baa1ca7f156af2c87498e9d749b619d61226
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-03-30 20:49:35 +0800

    CREATED: constants file

---
+++ src/constants/index.tsx
@@ -0,0 +1,115 @@
+import homeDecorOne from "/public/decor/home_decor_1.png";
+import homeDecorTwo from "/public/decor/home_decor_2.jpg";
+import homeDecorThree from "/public/decor/home_decor_3.jpg";
+import homeDecorFour from "/public/decor/home_decor_4.jpg";
+import homeDecorFive from "/public/decor/home_decor_5.jpg";
+
+import logoDarkEqual from "/public/logos/dark-equal-logo.webp";
+import logoDarkRealtor from "/public/logos/dark-realtor-logo.webp";
+import logoSereno from "/public/logos/sereno-logo.webp";
+
+export const navigationBarTitles = [
+    {
+        title: 'home',
+    },
+    {
+        title: 'meet the team',
+    },
+    {
+        title: 'search for homes',
+    },
+    {
+        title: 'our communities',
+    },
+    {
+        title: 'home valuation',
+    },
+    {
+        title: 'services',
+    },
+    {
+        title: 'home across america',
+    },
+    {
+        title: 'testimonials',
+    },
+    {
+        title: 'contact us',
+    },
+]
+
+export const homeDecorImages = [
+    {
+        image: homeDecorOne,
+    },
+    {
+        image: homeDecorTwo,
+    },
+    {
+        image: homeDecorThree,
+    },
+    {
+        image: homeDecorFour,
+    },
+    {
+        image: homeDecorFive,
+    },
+]
+
+export const sellingProcesses = [
+    {
+        stepNumber: 1,
+        stepTitle: "initial consultation and planning"
+    },
+    {
+        stepNumber: 2,
+        stepTitle: "devise & execute marketing plan"
+    },
+    {
+        stepNumber: 3,
+        stepTitle: "review offers & reach agreement with buyer"
+    },
+    {
+        stepNumber: 4,
+        stepTitle: "complete transaction process"
+    },
+    {
+        stepNumber: 5,
+        stepTitle: "after-sale service"
+    },
+]
+
+export const buyingProcesses = [
+    {
+        stepNumber: 1,
+        stepTitle: "initial consultation and planning",
+    },
+    {
+        stepNumber: 2,
+        stepTitle: "search for a home & get pre-approved",
+    },
+    {
+        stepNumber: 3,
+        stepTitle: "submit an offer",
+    },
+    {
+        stepNumber: 4,
+        stepTitle: "complete settlement process",
+    },
+
+]
+
+export const footerLogos = [
+    {
+        id: 1,
+        image: logoDarkEqual,
+    },
+    {
+        id: 2,
+        image: logoDarkRealtor,
+    },
+    {
+        id: 3,
+        image: logoSereno,
+    },
+]
